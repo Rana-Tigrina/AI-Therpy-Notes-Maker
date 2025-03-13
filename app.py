d9b71377b514e1a2a34a9611fd0b6c15dae57655
Munawwar Rana <96058430+Rana-Tigrina@users.noreply.github.com>
2025-03-13 22:04:19 +0530

    updated app.py with rendered html

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from api import api_bp
+from config import UPLOAD_FOLDER, logger
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 3600
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+    # Register Blueprints
+    app.register_blueprint(api_bp)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    logger.info("Application started.")
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
